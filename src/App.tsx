2a05bdec020d36f962d8f30e3d46af0a0acba0bd
Vijay Kaki <vkaki@Commonwealth.com>
2025-07-11 12:52:28 -0400

    UserOptions added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
 function App() {
+  const {user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [userOptions] = useState<Array<Schema["UserOptions"]["type"]>>([]);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -17,13 +20,31 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+  
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <br />
+      <h2>User Options</h2>
+      <button onClick={() => client.models.UserOptions.create({ DisplayFirstNameOnly: true, DisplayLastNameOnly: false })}>
+        Set User Options
+      </button>
+      <ul>
+        {userOptions.map((option) => (
+          <li key={option.id}>
+            DisplayFirstNameOnly: {option.DisplayFirstNameOnly ? "Yes" : "No"}, 
+            DisplayLastNameOnly: {option.DisplayLastNameOnly ? "Yes" : "No"}
+          </li>
         ))}
       </ul>
       <div>
@@ -33,6 +54,7 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+       <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
